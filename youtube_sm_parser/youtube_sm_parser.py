c6b6751ffcb7dcddd800143d167aa772ae6350d5
sharktamer <donohoe.shane@gmail.com>
2019-05-26 02:57:21 +0100

    implement output options

--- youtube_sm_parser/youtube_sm_parser.py
+++ youtube_sm_parser/youtube_sm_parser.py
@@ -2,10 +2,14 @@
 
 """Main module."""
 
+import os
 import sys
 import xmltodict
-from requests_futures.sessions import FuturesSession
 import argparse
+import json
+import yaml
+
+from requests_futures.sessions import FuturesSession
 
 
 def parse_args(args):
@@ -14,12 +18,14 @@ def parse_args(args):
 
     format_choices = ['json', 'lines', 'yaml']
     parser.add_argument('-f', '--format', choices=format_choices,
-                        default='lines', required=True)
+                        default='lines')
 
     parser.add_argument('-l', '--line_format', default='{title},{link}')
 
     default_input_fn = '~/.config/youtube_sm_parser/subscription_manager'
     parser.add_argument('-i', '--input', default=default_input_fn)
+
+    parser.add_argument('-w', '--workers', default=10, type=int)
 
     return parser.parse_args(args)
 
@@ -62,16 +68,27 @@ def feed_to_dicts(r, *args, **kwargs):
 
 
 def main():
-    opml_fn = sys.argv[1]
-    with open(opml_fn) as f:
+    args = parse_args(sys.argv[1:])
+
+    with open(os.path.expanduser(args.input)) as f:
         opml_dict = xmltodict.parse(f.read())
     feeds = extract_feeds(opml_dict)
 
-    session = FuturesSession(max_workers=50)
+    session = FuturesSession(max_workers=args.workers)
     futures = [session.get(f, hooks={'response': feed_to_dicts})
                for f in feeds]
     entry_lists = [f.result().data for f in futures]
     entries = [i for s in entry_lists for i in s]
+
+    if args.format == 'json':
+        output = json.dumps(entries, indent=4)
+    elif args.format == 'lines':
+        output = '\n'.join(format_dict(line, args.line_format)
+                           for line in entries)
+    elif args.format == 'yaml':
+        output = yaml.dump(entries)
+
+    print(output)
 
 
 if __name__ == '__main__':
